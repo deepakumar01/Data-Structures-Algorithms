2dfb0ca467cfe5c6138869ac8e9a75fa4046fef2
Deepak Kumar <2081deepakkumar@gmail.com>
2026-06-11 20:25:43 +0530

    Arrays practice questions

--- ARRAYS/SumOfSubarrays.java
+++ ARRAYS/SumOfSubarrays.java
@@ -1,24 +1,16 @@
 package ARRAYS;
 class SumOfSubarrays{
     static int subarraySum(int[] arr) {
-        
         int n = arr.length;
-        int result = 0, temp = 0;
-
-        // pick starting point
-        for (int i = 0; i < n; i++) {
-
-            // pick ending point
-            temp = 0;
-            for (int j = i; j < n; j++) {
-
-                // sum subarray between current starting 
-                // and ending points
-                temp += arr[j];
-                result += temp;
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            int currSum = 0;
+            for(int j = i; j < n; j++) {
+                currSum += arr[j];
+                sum += currSum;
             }
         }
-        return result;
+        return sum;
     }
 
     public static void main(String[] args) {
