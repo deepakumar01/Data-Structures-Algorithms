ce875237032597999821f492a95d380455c6479f
Deepak Kumar <2081deepakkumar@gmail.com>
2026-06-14 21:39:11 +0530

    Arrays practice questions

--- ARRAYS/NextPermutation.java
+++ ARRAYS/NextPermutation.java
@@ -25,6 +25,7 @@ class NextPermutation {
         
         // Find the element from the right 
         // that is greater than pivot
+        
         for (int i = n - 1; i > pivot; i--) {
             if (arr[i] > arr[pivot]) {
                 swap(arr, i, pivot);
