74be47fa6b2e8d3ddfdf2a7f19c90d0af7020334
Deepak Kumar <2081deepakkumar@gmail.com>
2026-06-23 20:35:15 +0530

    Arrays practice questions

--- ARRAYS/ThreeGreatCandidates.java
+++ ARRAYS/ThreeGreatCandidates.java
@@ -30,6 +30,7 @@ public class ThreeGreatCandidates {
         return Math.max(firstmax*secondmax*thirdmax,
         firstmax*firstmin*secondmin);
     }
+    
     public static void main(String[] args){
         int[] arr = { 10, 3, 5, 6, 20 };
         int res = maxProduct(arr);
