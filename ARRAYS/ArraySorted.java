5d62f660a73c33b6bc34e397078ae3f5b1cbcf8f
Deepak Kumar <2081deepakkumar@gmail.com>
2026-07-05 20:40:10 +0530

    Sorted array apporaches

---
+++ ARRAYS/ArraySorted.java
@@ -0,0 +1,56 @@
+package ARRAYS;
+
+import java.util.Arrays;
+
+public class ArraySorted {
+    static boolean isSorted(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++)
+            if (arr[i - 1] > arr[i])
+                return false;
+        return true;
+
+        //My approach
+        // int n = arr.length;
+        // int count =0;
+        // for(int i=1;i<n;i++){
+        //     if(arr[i]>=arr[i-1]){
+        //         count++;
+        //     }
+        // }
+        // if(count==n-1) return true;
+        // else return false;
+
+        //Another apporach
+        // for(int i=1;i<n;i++){
+        //     if(arr[i]>=arr[i-1]){
+
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        //Recursive approach
+    // static boolean isSortedHelper(int[] arr, int n){
+    //     if(n==0 || n==1) return true;
+    //     return arr[n-1]>=arr[n-2] && isSortedHelper(arr, n-1);
+    // }
+    // public boolean isSorted(int[] arr) {
+    //     return isSortedHelper(arr, arr.length);
+    // }
+
+        //Built-in Methods
+        // int[] copy = arr.clone();
+        // Arrays.sort(copy);
+        // return Arrays.equals(copy,arr);
+    }
+    public static void main(String[] args){
+        int[] arr = {10, 20, 30, 40, 50};
+        if (isSorted(arr))
+            System.out.print("true\n");
+        else
+            System.out.print("false\n");
+    }
+}
--- ARRAYS/RemoveDuplicatesInplace.java
+++ ARRAYS/RemoveDuplicatesInplace.java
@@ -1,29 +1,26 @@
 package ARRAYS;
-
 public class RemoveDuplicatesInplace {
 
     static int removeDuplicates(int[] nums) {
         int i = 0;
-
         for(int j = 1; j < nums.length; j++) {
             if(nums[i] != nums[j]) {
                 nums[i + 1] = nums[j];
                 i++;
             }
         }
-
         return i + 1;
-    }
 
         // Using HashSet (library -> import java.util.HashSet;)
         // HashSet<Integer> set = new HashSet<>();
         // int index = 0;
         // for(int num : nums) {
-        //     if(set.add(num)) {   // add() returns true if element is new
+        //     if(set.add(num)) { 
         //         nums[index++] = num;
         //     }
         // }
         // return index;
+    }
 
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 2, 3, 3, 4, 5, 5};
