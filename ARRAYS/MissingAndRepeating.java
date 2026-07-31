412bf05a05aa6cb0827bdf5625d562514ea1d1ed
Deepak Kumar <2081deepakkumar@gmail.com>
2026-07-31 21:36:52 +0530

    array approach

--- ARRAYS/MissingAndRepeating.java
+++ ARRAYS/MissingAndRepeating.java
@@ -4,29 +4,48 @@ import java.util.ArrayList;
 
 class MissingAndRepeating {
     static ArrayList<Integer> findTwoElement(int[] arr) {
+
+        // By Math Equations
         ArrayList<Integer> list = new ArrayList<>();
+        int n = arr.length;
+        long sumOfNatural = (long)n * (n + 1) / 2;
+        long sumOfSqNatural = (long)n * (n + 1) * (2 * n + 1) / 6;
+        long sumOfArr = 0;
+        long sumOfSqArr = 0;
+        for (int i = 0; i < n; i++) {
+            sumOfArr += arr[i];
+            sumOfSqArr += (long)arr[i] * arr[i];
+        }
+        long SimpleDiff = sumOfArr - sumOfNatural;      // x - y
+        long SqDiff = sumOfSqArr - sumOfSqNatural;      // x² - y²
+        long xplusy = SqDiff / SimpleDiff;              // x + y
+        int x = (int)((SimpleDiff + xplusy) / 2);          // repeating
+        int y = (int)(x - SimpleDiff);                  // missing
+        list.add(x);
+        list.add(y);
+        return list;
 
         // Brute Force Approach
-        int repeating = -1;
-        int missing = -1;
-        int n = arr.length;
-        for (int i = 1; i <= n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == i) {
-                    count++;
-                }
-            }
-            if (count == 2) {
-                repeating = i;
-            } 
-            else if (count == 0) {
-                missing = i;
-            }
-        }
-        list.add(repeating);
-        list.add(missing);
-        return list;
+        // int repeating = -1;
+        // int missing = -1;
+        // int n = arr.length;
+        // for (int i = 1; i <= n; i++) {
+        //     int count = 0;
+        //     for (int j = 0; j < n; j++) {
+        //         if (arr[j] == i) {
+        //             count++;
+        //         }
+        //     }
+        //     if (count == 2) {
+        //         repeating = i;
+        //     } 
+        //     else if (count == 0) {
+        //         missing = i;
+        //     }
+        // }
+        // list.add(repeating);
+        // list.add(missing);
+        // return list;
 
         // By Hash Array
         // ArrayList<Integer> list = new ArrayList<>();
@@ -72,25 +91,6 @@ class MissingAndRepeating {
         // list.add(missing);
         // return list;
 
-        // By Math Equations
-        // ArrayList<Integer> list = new ArrayList<>();
-        // int n = arr.length;
-        // long sumOfNatural = (long)n * (n + 1) / 2;
-        // long sumOfSqNatural = (long)n * (n + 1) * (2 * n + 1) / 6;
-        // long sumOfArr = 0;
-        // long sumOfSqArr = 0;
-        // for (int i = 0; i < n; i++) {
-        //     sumOfArr += arr[i];
-        //     sumOfSqArr += (long)arr[i] * arr[i];
-        // }
-        // long SimpleDiff = sumOfArr - sumOfNatural;      // x - y
-        // long SqDiff = sumOfSqArr - sumOfSqNatural;      // x² - y²
-        // long xplusy = SqDiff / SimpleDiff;              // x + y
-        // int x = (int)((SimpleDiff + xplusy) / 2);          // repeating
-        // int y = (int)(x - SimpleDiff);                  // missing
-        // list.add(x);
-        // list.add(y);
-        // return list;
         
     }
 
