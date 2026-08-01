06ac6d6428c71a10ed1c4f0a5d458056e5783e0e
Deepak Kumar <2081deepakkumar@gmail.com>
2026-08-01 22:58:05 +0530

    array approach

--- ARRAYS/MergeUnionSortedArrays.java
+++ ARRAYS/MergeUnionSortedArrays.java
@@ -7,13 +7,13 @@ import java.util.ArrayList;
 // import java.util.HashMap;
 public class MergeUnionSortedArrays {
     public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        ArrayList<Integer> list = new ArrayList<>();
         int n1 = a.length;
         int n2 = b.length;
-        ArrayList<Integer> list = new ArrayList<>();
         int i=0;
         int j=0;
         while(i<n1 && j<n2){
-            if(a[i]<=b[j]){
+            if(a[i]<b[j]){
                 if(list.size()==0 || list.get(list.size()-1)!=a[i]){
                     list.add(a[i]);
                 }
@@ -80,6 +80,45 @@ public class MergeUnionSortedArrays {
         // }
         // ArrayList<Integer> list = new ArrayList<>(map.keySet());
         // Collections.sort(list);
+        // return list;
+
+        // Detailed conditional comparison for both arrays
+        // ArrayList<Integer> list = new ArrayList<>();
+        // int n = a.length;
+        // int m = b.length;
+        // int i=0,j=0;
+        // while(i<n && j<m){
+        //     if(a[i]<b[j]){
+        //         if(list.size()==0 || list.get(list.size()-1)!=a[i]){
+        //             list.add(a[i]);
+        //         }
+        //         i++;
+        //     }
+        //     else if(a[i]>b[j]){
+        //         if(list.size()==0 || list.get(list.size()-1)!=b[j]){
+        //             list.add(b[j]);
+        //         }
+        //         j++;
+        //     }
+        //     else{
+        //         if(list.size()==0 || list.get(list.size()-1)!=a[i]){
+        //             list.add(a[i]);
+        //         }
+        //         i++;j++;
+        //     }
+        // }
+        // while(i<n){
+        //     if(list.size()==0 || list.get(list.size()-1)!=a[i]){
+        //         list.add(a[i]);
+        //     }
+        //     i++;
+        // }
+        // while(j<m){
+        //     if(list.size()==0 || list.get(list.size()-1)!=b[j]){
+        //         list.add(b[j]);
+        //     }
+        //     j++;
+        // }
         // return list;
     }
 
--- ARRAYS/SortedAndRotatedArray.java
+++ ARRAYS/SortedAndRotatedArray.java
@@ -4,12 +4,21 @@ public class SortedAndRotatedArray {
         int count = 0;
         int n = nums.length;
         for(int i=0;i<n;i++){
-            if(nums[(i+1)%n]<nums[i]){
+            if(nums[i]>nums[(i+1)%n]){
                 count++;
             }
             if(count>1) return false;
         }
         return true;
+        
+        // Using nums[n-1] comparison instead of nums[i]
+        // for(int i=1;i<=n;i++){
+        //     if(nums[i-1]>nums[i%n]) count++;
+        //     if(count>1){
+        //         return false;
+        //     }
+        // }
+        // return true;
 
         //Brute Force Approach
         // int n = nums.length;
