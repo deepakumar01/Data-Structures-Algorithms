0828a6a7fd3f5e9b457a0372407fcad938cebf0e
Deepak Kumar <2081deepakkumar@gmail.com>
2026-08-06 19:53:14 +0530

    array approach

--- ARRAYS/MaxConsecutiveBit01.java
+++ ARRAYS/MaxConsecutiveBit01.java
@@ -15,7 +15,6 @@ class MaxConsecutiveBit01{
         }
         return maxCount;
     }
-
     public static void main(String[] args) {
         int[] arr = {0, 1, 0, 1, 1, 1, 1};
         System.out.println(maxConsecBits(arr));
