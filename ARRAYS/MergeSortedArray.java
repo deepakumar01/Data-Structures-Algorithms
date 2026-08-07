3f77969ffccb060570327f5b190c731007c0f1a4
Deepak Kumar <2081deepakkumar@gmail.com>
2026-08-07 21:55:08 +0530

    array approach

--- ARRAYS/MergeSortedArray.java
+++ ARRAYS/MergeSortedArray.java
@@ -1,8 +1,65 @@
 package ARRAYS;
+import java.util.ArrayList;
 import java.util.Arrays;
 public class MergeSortedArray {
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        //With extra space using array
+        // int[] temp = new int[m+n];
+        // int i=0,j=0;
+        // int index=0;
+        // while(i<m && j<n){
+        //     if(nums1[i]<=nums2[j]){
+        //         temp[index++]=nums1[i];
+        //         i++;
+        //     }
+        //     else{
+        //         temp[index++]=nums2[j];
+        //         j++;
+        //     }
+        // }
+        // while(i<m){
+        //     temp[index++]=nums1[i];
+        //     i++;
+        // }
+        // while(j<n){
+        //     temp[index++]=nums2[j];
+        //     j++;
+        // }
+        // int idx=0;
+        // for(int k=0;k<temp.length;k++){
+        //     nums1[idx++] = temp[k];
+        // }
+
+        //With extra space using ArrayList
+        // ArrayList<Integer> list = new ArrayList<>();
+        // int i=0,j=0;
+        // while(i<m && j<n){
+        //     if(nums1[i]<=nums2[j]){
+        //         list.add(nums1[i]);
+        //         i++;
+        //     }
+            
+        //     else{
+        //         list.add(nums2[j]);
+        //         j++;
+        //     }
+            
+        // }
+        // while(i<m){
+        //     list.add(nums1[i]);
+        //     i++;
+        // }
+        // while(j<n){
+        //     list.add(nums2[j]);
+        //     j++;
+        // }
+        // int idx = 0;
+        // for(int nums:list){
+        //     nums1[idx++] = nums;
+        // }
+
         int left = m - 1;
         int right = 0;
         while (left >= 0 && right < n) {
@@ -36,36 +93,6 @@ public class MergeSortedArray {
         // }
         // while (j >= 0) {
         //     nums1[k--] = nums2[j--];
-        // }
-
-
-        // Brute force approach using extra space array of size m+n
-        // int[] temp = new int[m + n];
-        // int left = 0;
-        // int right = 0;
-        // int index = 0;
-        // while (left < m && right < n) {
-        //     if (nums1[left] <= nums2[right]) {
-        //         temp[index] = nums1[left];
-        //         left++;index++;
-        //     } else {
-        //         temp[index] = nums2[right];
-        //         right++;index++;
-        //     }
-        // }
-        // while (left < m) {
-        //     temp[index++] = nums1[left++];
-        // }
-        // while (right < n) {
-        //     temp[index++] = nums2[right++];
-        // }
-        // for (int i = 0; i < m + n; i++) {
-        //     nums1[i] = temp[i];
-        // }
-        // // if you need to put elements in nums1 and nums2 separately
-        // for(int i=0;i<m+n; i++){
-        //     if(i<m) nums1[i] = nums3[i];
-        //     else nums2[i-m] = nums3[i];
         // }
     }
 
